4082ae252808427cea81cf57bde05dbd6ddb8d2d
Derek Etman <dereketman@gmail.com>
2020-08-30 17:20:06 -0700

    feat: splash layout

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-
-
 export default function Splash() {
-    return (
-        <div className="splash-container">
-    
-        <h1> THIS IS SPLASH</h1>
-        </div>
-    )
-};
+	return (
+		<div className="splash-container">
+			<div className="splash-wrapper">
+				<h1 className="splash-header">
+					Delicious Meals for you and your furry friends to enjoy!
+				</h1>
+				<h3 className="splash-sub-header">
+					Pawsta is an organic delicious click to table meal kit service!‍
+				</h3>
+				<h3 className="splash-sub-header">
+					We'll provide you with all the necessary ingredients to make a gourmet
+					experience that both you and your dogs can share!
+				</h3>
+
+				<h3 className="splash-sub-header">
+					Tonight your dogs can sit at the dinner table.‍
+				</h3>
+				<button className="splash-cta-button"> ENJOY DINNER ON US!</button>
+			</div>
+		</div>
+	);
+}
